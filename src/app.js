780c3dc60dc8beb0e3228206f4e57caabaf0290e
Shyamsunder <b191564@rgukt.ac.in>
2024-10-23 19:25:46 +0530

    Multple Routes handling

--- src/app.js
+++ src/app.js
@@ -20,10 +20,63 @@
     //res.send("quesions");
  })
 
- app.use("/",(req,res)=>{
-    res.send("override");
+//  app.use("/",(req,res)=>{
+//     res.send("override");
+//  })
+
+
+ /*****************************Advanced Routings**************************** */
+
+ app.get("/admin",(req,res)=>{ //if we place ? after that letter that means it optional here it can work for admins and admin
+    res.send("admins page opens");
  })
 
+ app.get("/use+r",(req,res)=>{
+   res.send("users profile");
+ })
+app.get(/a/,(req,res)=>{
+   res.send("a page");
+})
+ app.get("/admins(role)?",(req,res)=>{
+    res.send("admin role dashboard opens");
+ })
+app.get("/*fly$",(req,res)=>{
+   res.send("sufix with fly"); //suffix end with fly
+})
  app.listen(3000,()=>{
     console.log("server running");
  })
+
+ /********************************************* Multiple route handlers*************************************** */
+ app.use("/customers",
+   (req,res)=>{
+      res.send("customers page");
+   },
+   (req,res)=>{
+      res.send("second customers page");
+   }
+ );
+
+ app.use("/clients",
+   (req,res,next)=>{
+      console.log("clients page");
+      next();
+      //res.send("hai");//error in the console because request already resolved if we want to resolve again the it will give eror in the console but not on the localhost
+   },
+   (req,res,next)=>{
+      console.log("2nd one");
+      next();
+      //res.send("2nd respons");
+   },
+   (req,res)=>{
+      res.send("3rd response");
+   }
+ )
+
+ app.use("/login",(req,res,next)=>{
+   console.log("login page");
+   res.send("login page");
+   //next();//eror it request already resolved
+ },(req,res)=>{
+   res.send("2nd login page");
+ });
