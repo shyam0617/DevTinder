9a815164bb5f6f91959e524590cea31dd34433a6
Shyamsunder <b191564@rgukt.ac.in>
2024-11-26 12:10:24 +0530

    requestRouter

--- src/Routes/request.js
+++ src/Routes/request.js
@@ -16,7 +16,26 @@ requestRouter.post("/request/send/:status/:toUserId",userauth, async (req,res)=>
         toUserId,
         status,
        })
+       const statusAllowed=["interested","ignored"];
+
+       if(!statusAllowed.includes(status))
+       return res.status(400).send("status not valid");
        
+       const isPresent=await ConnectionRequestModel.findOne({
+        $or:[
+            {
+               fromUserId,toUserId
+            },
+            {
+                fromUserId:toUserId,toUserId:fromUserId
+            }
+        ]
+       })
+       if(isPresent)
+       {
+         return res.status(400).send({message:"Connection request already exists"});
+       }
+
        const data=await ConnectionRequest.save();
        res.json({
         message:`Connection sent sucessfully`,
@@ -28,4 +47,40 @@ requestRouter.post("/request/send/:status/:toUserId",userauth, async (req,res)=>
         res.status(400).send("ERROR: "+err.message);
     }
 });
+
+requestRouter.post("/request/review/:status/:toUserId",userauth,  async(req,res)=>{
+   
+    try{ 
+         const loggeduser=req.user
+         const status=req.params.status;
+         const userid=req.params.toUserId;
+         console.log(userid);
+
+         const isallowed=["accepted","rejected"];
+
+         if(!isallowed.includes(status))
+         return res.status(400).json({message:"status is not valid"});
+         console.log(loggeduser._id);
+         const ConnectionRequest=await ConnectionRequestModel.findOne({
+            fromUserId:userid,
+            toUserId:loggeduser._id,
+            status:"interested",
+         });
+        
+         if(!ConnectionRequest)
+         {
+            res.status(400).json({message:"request not found"});
+         }
+         ConnectionRequest.status=status;
+         console.log(ConnectionRequest);
+
+         const data=await ConnectionRequest.save();
+         res.json({messaage:`successfully ${status} profile`});
+    }
+    catch(err)
+    {   
+        console.error(err);
+        res.status(400).json({message:"review is not possible"});
+    }
+})
 module.exports=requestRouter;
--- src/models/connectionrequest.js
+++ src/models/connectionrequest.js
@@ -22,6 +22,14 @@ const connectionRequestSchema=new mongoose.Schema(
         timestamps:true
     }
 );
+connectionRequestSchema.pre("save",function(next){
+   const connectionRequest=this;
+   //check if the fromuserid is same as to userid
+   if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+    throw new Error("Cannot send connection request to yourself");
+   }
+   next();
+})
 const ConnectionRequestModel=new mongoose.model(
     "ConnectionRequest",
     connectionRequestSchema
